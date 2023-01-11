77e58954a2eb1c0176ebb14961902bb2c027ee70
Kingsiyal524 <120108383+Kingsiyal524@users.noreply.github.com>
2023-01-12 02:13:42 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,9 +15,9 @@ actiontitle = '࿙⚜𝑨𝒏𝒚𝒂⚜࿚' // text who displays on the top of 
 global.headerText = "Anya by Pika" // text for header of menu //        
      
              //-------------👑Owner & vcard 👑----------\\             
-             global.ownernumber = ['918811074852']   // Owner number //
-             global.vcardowner = ['918811074852']   // vcard owner number //
-             global.ownername = "3.69_Pika"   // Owner name //
+             global.ownernumber = ['923099809063']   // Owner number //
+             global.vcardowner = ['923099809063']   // vcard owner number //
+             global.ownername = "3.69_AMaN_Siyal"   // Owner name //
              global.ytname = "YT: ONYXPIKA"   // Yt chanel name //
              global.socialm = "𝗚𝗶𝘁𝗵𝘂𝗯 : Pika4o4"   // Github or insta name //
              global.location = "India, Assam, Dibrugarh"   // Location //
