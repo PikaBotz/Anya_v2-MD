75bbb99e8e8c232eb3a70354b0eabf62b0354828
Faheem mod <122115102+Faheemxyz@users.noreply.github.com>
2023-02-03 22:03:06 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,43 +2,43 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.botname = "Queen Anya" 
-global.botLanguage = "default" // 𝗗𝗲𝗳𝗮𝘂𝗹𝘁 || 𝗘𝗻𝗴𝗹𝗶𝘀𝗵 || 𝗛𝗶𝗻𝗱𝗶 || 𝗔𝗿𝗮𝗯𝗶𝗰 \\
-global.author = "𝙋𝙄𝙆𝘼"  
+global.botname = "faheem v1" 
+global.botLanguage = "default" // 𝗗𝗲𝗳𝗮𝘂𝗹𝘁 || 𝗘𝗻𝗴𝗹𝗶𝘀𝗵 || 𝗛𝗶𝗻𝗱𝗶 || urdu || 𝗔𝗿𝗮𝗯𝗶𝗰 \\
+global.author = "faheem"  
 global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
-global.myweb = "https://github.com/PikaBotz" // You can put any link 
-global.waterMark = "×͜× King ✘ Pika™ ✓" // Your own watermark 
-global.headerText = "Anya by Pika" // text for header of menu         
-global.DocumentTitle = "MASTER⚔️PIKA" 
+global.myweb = "https://github.com/Faheemxyz" // You can put any link 
+global.waterMark = "×͜× King ✘ faheem ™ ✓" // Your own watermark 
+global.headerText = "faheem by mod" // text for header of menu         
+global.DocumentTitle = "MASTER⚔️faheem" 
 
 
-global.ownername = "𝕻𝖎𝖐𝖆♰𝕶𝖚𝖓"   // Owner name //        
-global.ownernumber = ['916900904828'] 
-global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_pika" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
-global.ytname = "YT: Ytname"   // Yt chanel name //
+global.ownername = "faheem"   // Owner name //        
+global.ownernumber = ['923067193190'] 
+global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_faheem" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
+global.ytname = "YT: faheem mod"   // Yt chanel name //
 global.continent = "Asia" // your continent name 
-global.region = "India" // your country name
-global.state = "Assam" // your state name
+global.region = "Pakistan" // your country name
+global.state = "faheemkhan" // your state name
 global.district = "Dibrugarh" // your district name
-global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
-global.instagramId = "3.69_pika" // Your insta ID
-global.email = "example@exmaple.com" // put your email here
+global.timezone = "Pakistan/dajal" // search on google if you don't know the timezone of your country //
+global.instagramId = "3.69_faheem" // Your insta ID
+global.email = "mf1133192@gmail.com" // put your email here
 
 
 //------------ 🌟 Premium Users 🌟-------------\\
-global.premium = ['916900******','917355******']  // available soon
-global.sudo = ['916900******','917355******']  // Co - Owners //
+global.premium = ['92306******','92336******']  // available soon
+global.sudo = ['92306******','92336******']  // Co - Owners //
 
 global.prefa = ['-','🥵','🔥']
 
 //--------Random bot name for Info section--------\\
-name16 = "𓄂⍣⃝ 🇦𝚗𝚢𝚊 𓆩×͜×𓆪ꪾ️"
-name17 = "*Λ⃝ͷ🇾Λ 𓆩×͜×𓆪ꪾ*"
-name18 = "*Λ⃝ͷʏΛ 𓆩×͜×𓆪ꪾ*"
+name16 = "𓄂⍣⃝ faheem 𓆩×͜×𓆪ꪾ️"
+name17 = "*faheem 𓆩×͜×𓆪ꪾ*"
+name18 = "*faheem 𓆩×͜×𓆪ꪾ*"
 
 //--------Random footer message-----------\\
-footer1 = `𓄂⍣⃝🇦𝚗𝚢𝚊ㅤꪜ⃢𝟤ㅤ𓆩×͜×𓆪ꪾ `
-footer2 = `Λ⃝ͷʏΛㅤꪜ⃢𝟤ㅤ𓆩×͜×𓆪ꪾ`
+footer1 = `𓄂⍣⃝faheem 𝟤ㅤ𓆩×͜×𓆪ꪾ `
+footer2 = `Λfaheemㅤꪜ⃢𝟤ㅤ𓆩×͜×𓆪ꪾ`
 
 //--------Left message "Nikal msg"--------\\
 nikalmsg1 = 'Ye galiye ye choubara, yaha dikhna na dubara 😚🖕🏻' 
