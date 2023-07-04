e6f84cbd36cd20cf89c706f62b704bcf8832a35d
Ziddeveer555 <117192392+Ziddeveer555@users.noreply.github.com>
2023-07-04 08:59:36 +0500

    Update config.js

--- config.js
+++ config.js
@@ -22,9 +22,9 @@ global.lApiKey = {// if this API key get expired then please go to https://api.l
 }
 
 
-global.botname = "Queen Anya" 
-global.author = "Enter Author Name Here"  
-global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
+global.botname = "MUZAMIL BOT" 
+global.author = "MUZAMIL HUSSAIN"  
+global.packname = "MUZAMIL HUSSAIN✯𝘽𝙊𝙏"  
 global.myweb = "Enter Your Any Website Profile Link Here"
 global.footer = "©\t" + "Enter Your Watermark Here"
 global.prefa = ['-'] // single prefix
@@ -33,8 +33,8 @@ global.socialText = `*⪧⪢ Please join this group for by bot's announcement.*`
 global.socialLink = `*⪧⪢ https://chat.whatsapp.com/HshHS6kHF9NHnA9lfMwwSM 💝*`
 
 
-global.ownername = "Enter Owner Name Here"    
-global.ownernumber = ["918811074852"]
+global.ownername = "MUZAMIL HUSSAIN"    
+global.ownernumber = ["923402450462"]
 global.ytname = "YT: Enter Your Channel Name Here"
 global.continent = "Asia" // your continent name 
 global.region = "Enter Your Country Name Here"
