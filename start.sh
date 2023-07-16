6f529c58bf5490b4c7ba85bab9225ee009c72b30
Pika-Kun 3 <121213527+PikaBotz@users.noreply.github.com>
2023-07-16 11:00:31 +0530

    patch #3 for v2.0.8

--- start.sh
+++ start.sh
@@ -1,6 +1,17 @@
 rm -rf Anya-Session
 rm -rf An*on
 mkdir Anya-Session
-mv *creds*json* creds.json
+latest_file=$(ls -t creds-*.json 2>/dev/null | head -n1)
+if [ -n "$latest_file" ]; then 
+  file_extension="${latest_file##*.}"
+  new_filename="creds.${file_extension}"
+if [ -e "creds.json" ]; then
+rm creds.json
+fi
+mv "$latest_file" creds.json
+  echo "🚀 Renamed $latest_file to creds.json because you used wrong filename."
+else
+  echo "Valid creds.json ✅"
+fi
 cp -r creds.json Anya-Session
 yarn starts
