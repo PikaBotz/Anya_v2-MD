4c3766d1f2cc599c7d6b84de15765d4ca7801e2a
Tomiwa Fadare <51441197+Razhfad@users.noreply.github.com>
2023-07-25 10:36:34 +0100

    Update config.js

--- config.js
+++ config.js
@@ -22,27 +22,27 @@ global.lApiKey = {// if this API key get expired then please go to https://api.l
 }
 
 
-global.botname = "Queen Anya" 
-global.author = "Enter Author Name Here"  
-global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
-global.myweb = "Enter Your Any Website Profile Link Here"
-global.footer = "©\t" + "Enter Your Watermark Here"
-global.prefa = ['-'] // single prefix
+global.botname = "Razhfad's tool" 
+global.author = "Night Hawk"  
+global.packname = "Pig Benis"  
+global.myweb = "twitter.com/a_sad_nerd"
+global.footer = "©\t" + "Razhfad"
+global.prefa = ['!'] // single prefix
 global.themeemoji = "🎐"
 global.autoBio = "$mode : $time ⌚ || $date 🗓️ || $botname" // Leave it as it is if you didn't understood!
-global.socialText = `*⪧⪢ Please join this group for by bot's announcement.*`
-global.socialLink = `*⪧⪢ https://chat.whatsapp.com/HshHS6kHF9NHnA9lfMwwSM 💝*`
+global.socialText = `*⪧⪢ wa.me/2349093924119 text me if you've got ideas.*`
+global.socialLink = `*⪧⪢ wa.me/2349093924119 💝*`
 
 
-global.ownername = "Enter Owner Name Here"    
-global.ownernumber = ["918811074852"]
+global.ownername = "Razhfad"    
+global.ownernumber = ["2349093924119"]
 global.ytname = "YT: Enter Your Channel Name Here"
-global.continent = "Asia" // your continent name 
-global.region = "Enter Your Country Name Here"
-global.state = "Enter Your State Name Here" // your state name
-global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
-global.instagramId = "Enter Your Insta ID Here"
-global.email = "Enter Your Email ID Here"
+global.continent = "Africa" // your continent name 
+global.region = "Nigeria"
+global.state = "Oyo state" // your state name
+global.timezone = "Africa/Lagos" // search on google if you don't know the timezone of your country //
+global.instagramId = "one_sad_nerd"
+global.email = "fadaretomiwa99@gmail.com"
 
 
 // Greeting messages
