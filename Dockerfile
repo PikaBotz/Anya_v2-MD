75d328598a30f4b20864ec7448f2a3daae145ace
olduser3 <jetushackk@usa.com>
2023-08-07 21:09:15 +0530

    Updating........

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM node:lts-buster
+
+RUN apt-get update && \
+  apt-get install -y \
+  ffmpeg \
+  imagemagick \
+  webp && \
+  apt-get upgrade -y && \
+  npm i pm2 -g && \
+  rm -rf /var/lib/apt/lists/*
+
+COPY package.json .
+
+RUN yarn install
+
+COPY . .
+
+CMD ["pm2-runtime", "."]
---
+++ docs/script.js
@@ -0,0 +1,41 @@
+function redirect(url) {
+    window.location.href = url;
+  }
+
+
+
+
+  // if we want to urls logic in js so 
+/*
+
+  
+
+  const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    redirectToURL(index + 1);
+  });
+});
+
+function redirectToURL(buttonIndex) {
+  const urlMap = {
+    1: 'https://www.example.com/button1-url',
+    2: 'https://www.example.com/button2-url',
+    3: 'https://www.example.com/button3-url',
+    4: 'https://www.example.com/button4-url',
+  };
+
+  const url = urlMap[buttonIndex];
+  if (url) {
+    redirect(url);
+  }
+}
+
+
+function redirect(url) {
+  window.location.href = url;
+}
+
+ 
+*/
