589133f0db4b6495b4d155d1079eb4ef83e7a8d3
olduser3 <jetushackk@usa.com>
2023-08-07 20:04:15 +0530

    Updating........

---
+++ replit.nix
@@ -0,0 +1,17 @@
+{ pkgs }: {
+    deps = [
+        pkgs.nodejs-16_x
+        pkgs.libwebp
+        pkgs.python
+	      pkgs.nodePackages.typescript
+        pkgs.libuuid
+        pkgs.ffmpeg
+        pkgs.imagemagick  
+        pkgs.wget
+        pkgs.git
+        pkgs.nodePackages.pm2
+    ];
+  env ={
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid];
+  };
+}
