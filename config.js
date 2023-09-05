148aa5e2b6b796d437b55399c811a19558754b3e
kartick0073 <85337412+kartick0073@users.noreply.github.com>
2023-09-05 14:28:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -22,11 +22,11 @@ global.lApiKey = {// if this API key get expired then please go to https://api.l
 }
 
 
-global.botname = "Queen Anya" 
-global.author = "Enter Author Name Here"  
-global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
+global.botname = "Queen Mickey" 
+global.author = "Kartick Basak"  
+global.packname = "𝙌𝙐𝙀𝙀𝙉✯Mickey✯𝘽𝙊𝙏"  
 global.myweb = "Enter Your Any Website Profile Link Here"
-global.footer = "©\t" + "Enter Your Watermark Here"
+global.footer = "©\t" + "karxixk73"
 global.prefa = ['-'] // single prefix
 global.themeemoji = "🎐"
 global.autoBio = "$mode : $time ⌚ || $date 🗓️ || $botname" // Leave it as it is if you didn't understood!
@@ -34,15 +34,15 @@ global.socialText = `*⪧⪢ Please join this group for by bot's announcement.*`
 global.socialLink = `*⪧⪢ https://chat.whatsapp.com/HshHS6kHF9NHnA9lfMwwSM 💝*`
 
 
-global.ownername = "Enter Owner Name Here"    
-global.ownernumber = ["918811074852"]
-global.ytname = "YT: Enter Your Channel Name Here"
+global.ownername = "Kartick Basak"    
+global.ownernumber = ["918392010527"]
+global.ytname = "YT: Black Pirates Occultists"
 global.continent = "Asia" // your continent name 
-global.region = "Enter Your Country Name Here"
-global.state = "Enter Your State Name Here" // your state name
-global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
-global.instagramId = "Enter Your Insta ID Here"
-global.email = "Enter Your Email ID Here"
+global.region = "India"
+global.state = "West Bengal" // your state name
+global.timezone = "Asia/Dhupguri" // search on google if you don't know the timezone of your country //
+global.instagramId = "karxixk_73"
+global.email = "evilfinix28@gmail.com"
 
 
 // Greeting messages
