be0d55da7f71b12b4e71c0132ff21d84a88a4c42
Teamolduser <120130487+Teamolduser@users.noreply.github.com>
2023-11-08 16:37:45 +0000

    Improvement

--- replit.nix
+++ replit.nix
@@ -1,6 +1,18 @@
 { pkgs }: {
-  deps = [
-    pkgs.ffmpeg.bin
-    pkgs.yarn
+    deps = [
+      
+        pkgs.nodejs-18_x
+        pkgs.libwebp
+	      pkgs.nodePackages.typescript
+        pkgs.libuuid
+        pkgs.ffmpeg
+        pkgs.imagemagick  
+        pkgs.wget
+        pkgs.git
+        pkgs.nodePackages.pm2
+        pkgs.yarn
   ];
+  env ={
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid];
+  };
 }
