5e5a23892c1a70d4aad685926d94f8f1f18552ee
saeedamanat <139762734+saeedamanat@users.noreply.github.com>
2024-01-15 21:49:56 +0500

    Update config.js
    *Join WhatsApp Group for more Courses*
    https://whatsapp.com/channel/0029VaCsTB8A89Me8ZpM8e1k
    
    ENJOY

--- config.js
+++ config.js
@@ -63,23 +63,23 @@ global.message = {
 },
 
 module.exports = {
-  botname: process.env.BotName || "Queen Anya", 
-  author: process.env.Author || "@PikaBotz",
-  packname: process.env.PackName || "Queen Anya v2 MD",
+  botname: process.env.BotName || "Dana Ashiq", 
+  author: process.env.Author || "@Danàashiq",
+  packname: process.env.PackName || "Dana Ashiq MD",
   socialLink: process.env.Web || "https://github.com/PikaBotz",
-  footer: process.env.Footer || "© Queen Anya Bot",
+  footer: process.env.Footer || "© Dana Ashiq Bot",
   prefa: process.env.Prefix || ['-'],
   themeemoji: process.env.ThemeEmoji || "🎐",
-  ownername: process.env.Owner_Name || "Pika~Kun",
-  ownernumber: process.env.Owner_Number || "916900904828",
-  instagramId: process.env.Insta || "8.08_only_mine",
+  ownername: process.env.Owner_Name || "Dana Ashiq",
+  ownernumber: process.env.Owner_Number || "923208061149",
+  instagramId: process.env.Insta || "dana_ashiq",
   warns: process.env.Warns_Limits || 3,
   mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-  welcome: process.env.Welcome_Msg || '*@$user* joined this group today as $membersth member.\n\n_$prefix welcome off to disable this message._',
+  welcome: process.env.Welcome_Msg || '*@$user* joined this group today as $membersth member. if you like this group join our website for service  https://danapakpanel.store/ ✅✅ \n\n_$prefix welcome off to disable this message._',
   left: process.env.Left_Msg || 'Ex-member *@$user* is no longer available in this group chat.\n\n_$prefix goodbye off to disable this message._',
   promote: process.env.Promote_Msg || '*@$user* has been promoted as an admin in this group.\n\n_$prefix promotem off to disable this message._',
   demote: process.env.Demote_Msg || '*@$user* has been demoted to a member in this group.\n\n_$prefix demotem off to disable this message._',
-  sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID", 
+  sessionId: process.env.SESSION_ID || "Y1Zyd05_Queen-Anya_ROWc=", 
   image_1: readFileSync('./lib/Assets/image_1.jpg'), // Thumbnail for allmenu command
   image_2: readFileSync('./lib/Assets/image_2.jpg'), // null image
   image_3: readFileSync("./lib/Assets/image_3.jpg"), // Thumbnail for Dashboard
@@ -105,19 +105,19 @@ module.exports = {
 
 
 // Ignore them 👇🏻
-global.botname = process.env.BotName || "Queen Anya" 
-global.author = process.env.Author || "@PikaBotz" 
-global.packname = process.env.PackName || "Queen Anya v2 MD" 
+global.botname = process.env.BotName || "Dana Ashiq" 
+global.author = process.env.Author || "@Dana Ashiq" 
+global.packname = process.env.PackName || "Dana Ashiq MD" 
 global.myweb = process.env.Web || "https://github.com/PikaBotz" 
-global.footer = process.env.Footer || "© Queen Anya Bot" 
+global.footer = process.env.Footer || "© Dana Ashiq Bot" 
 global.prefa = process.env.Prefix || ['-'] 
 global.themeemoji = process.env.ThemeEmoji || "🎐" 
-global.ownername = process.env.Owner_Name || "Pika~Kun" 
-global.ownernumber = process.env.Owner_Number || "916900904828" 
-global.adress = process.env.Continent || "Asia, India, Assam" 
-global.timezone = process.env.TimeZone || "Asia/Kolkata" 
-global.instagramId = process.env.Insta || "8.08_only_mine" 
-global.email = process.env.Email_Id || "example@example.com" 
+global.ownername = process.env.Owner_Name || "Dana Ashiq" 
+global.ownernumber = process.env.Owner_Number || "923208061149" 
+global.adress = process.env.Continent || "Asia, Pakistan, lahore" 
+global.timezone = process.env.TimeZone || "Asia/Karachi" 
+global.instagramId = process.env.Insta || "dana_ashiq" 
+global.email = process.env.Email_Id || "danaashiq@gmail.com" 
   
 //--------------- Tip ----------------\\
 global.Tips = [
