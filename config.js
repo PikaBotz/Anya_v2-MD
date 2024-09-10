5ee37372341ce9f527790065c33b08c4dad28b3a
Mohan22685 <jiniwase44@gmail.com>
2024-09-11 02:49:01 +0300

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ module.exports = {
      * owner details and parameters
      */
     ownername: process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
+    ownernumber: process.env.Owner_Number || "256752612901",
     instagramId: process.env.Insta || "3.69_pika",
 
     /**
@@ -29,8 +29,8 @@ module.exports = {
     groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGtnX",
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
-    mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    mongoUrl: process.env.MongoDB || "mongodb+srv://botm91799:<4POGsbrjjaQylyhT>@cluster0.gijan.mongodb.net/",
+    sessionId: process.env.SESSION_ID || "dDd2bXI_Queen-Anya_ydHY=",
 
     /**
      * thumbnails and medias
