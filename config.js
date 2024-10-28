d92f236c991b4e845b363817acbc3bfa10c954f8
ALPHAMALE-C <oyerindeadebukola11@gmail.com>
2024-10-28 21:27:16 +0100

    Update config.js

--- config.js
+++ config.js
@@ -16,8 +16,8 @@ module.exports = {
     /**
      * owner details and parameters
      */
-    ownername: process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
+    ownername: process.env.Owner_Name || "Ex-Ryden",
+    ownernumber: process.env.Owner_Number || "2348149825074",
     instagramId: process.env.Insta || "3.69_pika",
 
     /**
@@ -29,8 +29,8 @@ module.exports = {
     groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGtnX",
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
-    mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    mongoUrl: process.env.MongoDB || "mongodb+srv://Exryden:Alpha102($$)@cluster0.jjnlg.mongodb.net/",
+    sessionId: process.env.SESSION_ID || "djYyMnR_AN_YA_6eXA=",
 
     /**
      * thumbnails and medias
